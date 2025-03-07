5dfc845aca295f080c9c92a9e42bf321305a06f1
Munch42 <41879159+Munch42@users.noreply.github.com>
2025-03-07 11:35:09 -0700

    Added ex3

--- jsx-exercise/src/App.js
+++ jsx-exercise/src/App.js
@@ -1,5 +1,9 @@
+import About from './About';
 import './App.css';
 import React from 'react';
+import Home from './Home';
+import Contact from './Contact';
+import EngineeringTopics from './EngineeringTopics';
 
 function App() {
 
@@ -14,6 +18,10 @@ function App() {
       <p>Current Year: {currentYear}</p>
             
       <p>{isLoggedIn ? "Welcome back!" : "Please log in."}</p>
+      <Home title={"Home Page"} description={"Welcome to our website."} />
+      <About title={"About Us"} description={"We are passionate about delivering quality experiences."} />
+      <Contact title={"Contact Us"} description={"Feel free to reach out to us via email or phone."}/>
+      <EngineeringTopics />
     </div>
   );
 }
